c9b0d932f1ec2f5ff1489f98c3764182a181e84e
PranshiGarg9420 <75732814+PranshiGarg9420@users.noreply.github.com>
2022-03-08 16:21:00 +0530

    Add files via upload

---
+++ coins.js
@@ -0,0 +1,29 @@
+AFRAME .registerComponent('coins',{
+    init: function(){
+        for(var i=1; i<=10;i++){
+            const id=`coin${i}`;
+
+            const posX= Math.random()*100+ -50;
+            const posY= Math.random()*5 +5 ;
+            const posZ= Math.random()*60 + -40;
+            const position={x:posX, y:posY, z:posZ};
+            this.createCoin(id,position);
+        }
+    },
+    createCoin: function(id,position){
+        const coinsModel= document.querySelector('#coinModel');
+        const coin1= document.createElement("a-entity");
+
+        coin1.setAttribute('id',id);
+        coin1.setAttribute('position', position);
+        coin1.setAttribute('material','color',"#ff9100");
+        coin1.setAttribute('geometry',{primitive:'circle', radius:1});
+        coin1.setAttribute('animation',{
+            property: "rotation",
+            to:"0 360 0",
+            dur: 1000,
+            loop:"true"
+        });
+        coinsModel.appendChild(coin1);
+    }
+});
